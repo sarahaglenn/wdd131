eac33f30abf7bda0f00be0026a7e8c944eb75907
sarahaglenn <sar00@byui.edu>
2024-06-01 19:19:50 +0200

    Final project - culture page

--- project/scripts/project.js
+++ project/scripts/project.js
@@ -2,9 +2,15 @@ const today = new Date();
 document.getElementById("currentyear").innerHTML = today.getFullYear();
 document.querySelector("#lastModified").innerHTML = `Last Modified: ${new Date(document.lastModified).toLocaleString()}`;
 
-const newsButton = document.getElementById('#subscribe');
+const newsButton = document.getElementById("subscribe");
+
 if (newsButton) {
     newsButton.addEventListener("click", function() {
         window.open("newsletter.html", "_blank");
-    });
+        });
 }
+
+
+// newsButton.addEventListener("click", function() {
+//     window.open("newsletter.html", "_blank");
+//     });
